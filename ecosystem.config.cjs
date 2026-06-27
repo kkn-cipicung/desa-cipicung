810440767ec2ef3221545fd806e4e42b68b6e920
aryarobyy <robyaryanata12345@gmail.com>
2026-06-27 17:10:20 +0700

    feat: fix lint

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,12 +1,14 @@
 // ecosystem.config.cjs
 module.exports = {
-  apps: [{
-    name: 'desa-cipicung',
-    script: 'build/index.js',
-    env: {
-      PORT: 3000,
-      HOST: '0.0.0.0',
-      NODE_ENV: 'production'
-    }
-  }]
-}
+	apps: [
+		{
+			name: 'desa-cipicung',
+			script: 'build/index.js',
+			env: {
+				PORT: 3000,
+				HOST: '0.0.0.0',
+				NODE_ENV: 'production'
+			}
+		}
+	]
+};
