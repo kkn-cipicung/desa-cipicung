cf254f43473e77cd2943a6ffb84321b25f898e6f
aryarobyy <robyaryanata12345@gmail.com>
2026-06-27 17:22:39 +0700

    feat: fix cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,7 +3,8 @@ module.exports = {
 	apps: [
 		{
 			name: 'desa-cipicung',
-			script: 'build/index.js',
+			script: './build/index.js',
+			cwd: '/home/ilham/app/desa-cipicung',
 			env: {
 				PORT: 3000,
 				HOST: '0.0.0.0',
